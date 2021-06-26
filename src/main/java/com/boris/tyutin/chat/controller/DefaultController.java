1944afc54b07924f346147d28b769ed161ae05da
Tyutin Boris <boris919323@gmail.com>
2021-06-26 18:26:48 +0300

    change index

--- src/main/java/com/boris/tyutin/chat/controller/DefaultController.java
+++ src/main/java/com/boris/tyutin/chat/controller/DefaultController.java
@@ -1,13 +1,26 @@
 package com.boris.tyutin.chat.controller;
 
+import com.boris.tyutin.chat.model.entity.Message;
+import com.boris.tyutin.chat.model.service.MessageService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class DefaultController {
 
-    @RequestMapping("/")
-    public String getIndex() {
+    private final MessageService messageService;
+
+    public DefaultController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping("/")
+    public String getMessages(Model model) {
+        List<Message> allMessage = messageService.findAllMessage();
+        model.addAttribute("messages", allMessage);
         return "index";
     }
 }
