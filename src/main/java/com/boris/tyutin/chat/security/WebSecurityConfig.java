84481bb0a816631fa2a31edae3af56089502a388
Tyutin Boris <boris919323@gmail.com>
2021-06-26 21:00:45 +0300

    add default login

---
+++ src/main/java/com/boris/tyutin/chat/security/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.boris.tyutin.chat.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("http://localhost:8080/api/messages", true)
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
