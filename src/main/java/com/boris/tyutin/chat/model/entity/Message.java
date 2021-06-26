91b87ab74ac35e794b94e749010f44fb31e65b84
Tyutin Boris <boris919323@gmail.com>
2021-06-26 16:18:39 +0300

    add entity and connect to db

---
+++ src/main/java/com/boris/tyutin/chat/model/entity/Message.java
@@ -0,0 +1,39 @@
+package com.boris.tyutin.chat.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+import javax.persistence.FetchType;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "messages")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @Column
+    private String title;
+
+    @Column(length = 2024)
+    private String text;
+}
---
+++ src/main/java/com/boris/tyutin/chat/model/entity/User.java
@@ -0,0 +1,42 @@
+package com.boris.tyutin.chat.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.OneToMany;
+import javax.persistence.FetchType;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "author")
+    private List<Message> messages;
+}
