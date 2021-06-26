687eaade4a25244b2494b3e28444e3f1222dfd17
Tyutin Boris <boris919323@gmail.com>
2021-06-26 16:50:41 +0300

    add migration

---
+++ src/main/java/com/boris/tyutin/chat/model/service/MessageService.java
@@ -0,0 +1,21 @@
+package com.boris.tyutin.chat.model.service;
+
+import com.boris.tyutin.chat.model.entity.Message;
+import com.boris.tyutin.chat.model.repository.MessageRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public List<Message> findAllMessage() {
+        return messageRepository.findAll();
+    }
+}
